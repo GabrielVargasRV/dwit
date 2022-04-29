103c32f6003bc7a038d8a0a13eb4e410d4e66f27
Gabriel Vargas <josegabrielrosariovarg@gmail.com>
2022-04-29 12:24:30 -0400

    react helmet

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -16,18 +16,18 @@ import {
     Heart,
     SizeGuide,
     ShareBtn
-} from './styled-components'
+} from './styled-components';
 import { useParams } from 'react-router-dom';
 import styles from "./styles.module.css";
 import ReletedProduct from '../../components/product/index';
 import CartServices from "../../services/cart.services";
 import ProductsServices from "../../services/products.services";
-import LoadingPage from '../loading/index'
-import { toast } from 'react-toastify'
-import Notification from '../../components/notification/index'
-import LoadingSpinner from '../../components/loadingSpinner/index'
+import LoadingPage from '../loading/index';
+import { toast } from 'react-toastify';
+import Notification from '../../components/notification/index';
 import SizeGuideModal from '../../components/modals/sizeGuide/index';
 import { connect } from "react-redux";
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import useShare from "../../hooks/useShare";
 
 
@@ -84,66 +84,73 @@ const Product = ({favorites}) => {
     if (loading) return <LoadingPage />
 
     return (
-        <Container>
-            <Content>
-                <Center>
-                    <Photo bg={data.image} >
-                        <Heart
-                            onClick={handleOnClick}
-                            isLiked={isLiked}
-                            className={[isLiked ? "fas fa-heart" : "far fa-heart", "heart-icon"]}
-                        ></Heart>
-                    </Photo>
-                    <Info>
-                        <InfoTop>
-                            <Tittle>{data.title}</Tittle>
-                            <Description>{data.description}</Description>
-                            <Sizes>
-                                {data.sizes.map((size, index) => (
-                                    <button className={`${styles.size} ${activeSize === index && styles.current_size}`} key={index} onClick={() => setActiveSize(index)} >
-                                        {size.size}
-                                        <span>${size.price}</span>
-                                    </button>
-                                ))}
-                            </Sizes>
-                        </InfoTop>
-                        <div>
-                            <SizeGuide onClick={() => setModal(<SizeGuideModal/>)} >
-                                <i className="fas fa-ruler"></i> 
-                                <span>Size guide</span>
-                            </SizeGuide>
-                            <Price>${data.sizes[activeSize].price}</Price>
-                            <AddToCartBtn onClick={() => { handleAddProductToCart() }} >
-                                {addingToCart ? (
-                                    // <LoadingSpinner />
-                                    'Adding...'
-                                ) : (
-                                    'Add to cart'
-                                )}
-                            </AddToCartBtn>
-                            <ShareBtn
-                                onClick={() => share(data.title,'',`https://donewithit-511d4.web.app/product/${id}`)}
-                            >
-                                <i className="fas fa-share"></i>
-                            </ShareBtn>
-                        </div>
-                    </Info>
-                </Center>
-                <h2 style={{ marginTop: '10px' }} >Related Products</h2>
-            </Content>
-            <RelatedProducts>
-                {related.map(product => (
-                    <ReletedProduct key={product.id} data={product} />
-                ))}
-            </RelatedProducts>
+        <HelmetProvider>
+            <Container>
+                <Helmet>
+                    <title>DWIT - {data.title}</title>
+                    <meta property="og:image:url" content={`"${data.image}"`} />
+                    <meta property="og:title" content={`"DWIT - ${data.title}"`} />
+                </Helmet>
+                <Content>
+                    <Center>
+                        <Photo bg={data.image} >
+                            <Heart
+                                onClick={handleOnClick}
+                                isLiked={isLiked}
+                                className={[isLiked ? "fas fa-heart" : "far fa-heart", "heart-icon"]}
+                            ></Heart>
+                        </Photo>
+                        <Info>
+                            <InfoTop>
+                                <Tittle>{data.title}</Tittle>
+                                <Description>{data.description}</Description>
+                                <Sizes>
+                                    {data.sizes.map((size, index) => (
+                                        <button className={`${styles.size} ${activeSize === index && styles.current_size}`} key={index} onClick={() => setActiveSize(index)} >
+                                            {size.size}
+                                            <span>${size.price}</span>
+                                        </button>
+                                    ))}
+                                </Sizes>
+                            </InfoTop>
+                            <div>
+                                <SizeGuide onClick={() => setModal(<SizeGuideModal/>)} >
+                                    <i className="fas fa-ruler"></i> 
+                                    <span>Size guide</span>
+                                </SizeGuide>
+                                <Price>${data.sizes[activeSize].price}</Price>
+                                <AddToCartBtn onClick={() => { handleAddProductToCart() }} >
+                                    {addingToCart ? (
+                                        // <LoadingSpinner />
+                                        'Adding...'
+                                    ) : (
+                                        'Add to cart'
+                                    )}
+                                </AddToCartBtn>
+                                <ShareBtn
+                                    onClick={() => share(data.title,'',`https://donewithit-511d4.web.app/product/${id}`)}
+                                >
+                                    <i className="fas fa-share"></i>
+                                </ShareBtn>
+                            </div>
+                        </Info>
+                    </Center>
+                    <h2 style={{ marginTop: '10px' }} >Related Products</h2>
+                </Content>
+                <RelatedProducts>
+                    {related.map(product => (
+                        <ReletedProduct key={product.id} data={product} />
+                    ))}
+                </RelatedProducts>
 
-            {modal && (
-                <div className={styles.modal}>
-                    <SizeGuideModal close={() => setModal(null)} />
-                </div>
-            )}
+                {modal && (
+                    <div className={styles.modal}>
+                        <SizeGuideModal close={() => setModal(null)} />
+                    </div>
+                )}
 
-        </Container>
+            </Container>
+        </HelmetProvider>
     )
 }
 
